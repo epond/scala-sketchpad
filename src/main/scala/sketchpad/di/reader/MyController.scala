a8cb2374cef0924c358af1d15933b99135a6f13c
epond <epond@users.noreply.github.com>
2014-03-02 11:57:32 +0000

    comment

--- src/main/scala/sketchpad/di/reader/MyController.scala
+++ src/main/scala/sketchpad/di/reader/MyController.scala
@@ -2,6 +2,13 @@ package sketchpad.di.reader
 
 import scalaz._
 
+/**
+ * To use the reader monad for dependency injection, we define a function whose input
+ * is the dependency and wrap the function in a scalaz.Reader to give us map and flatMap.
+ * The return type of myMethod is a decorated Myservice => String that we can use in
+ * for comprehensions.
+ * The actual injection of the dependency (MyService) is deferred up to a higher layer.
+ */
 object MyController {
   def myMethod: Reader[MyService, String] = Reader {
     myService => "Hello " + myService.doStuff
