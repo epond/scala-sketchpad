ab097730a3e12f055997e9f8f9d5637c39bb850b
epond <epond@users.noreply.github.com>
2014-03-03 22:52:43 +0000

    Fix intermittent test failure

--- src/test/scala/sketchpad/forcomp/ForComprehensionsSpec.scala
+++ src/test/scala/sketchpad/forcomp/ForComprehensionsSpec.scala
@@ -4,6 +4,7 @@ import org.specs2.mutable.Specification
 import scala.concurrent._
 import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
+import java.util.concurrent.TimeUnit
 
 class ForComprehensionsSpec extends Specification {
   val f1 = Future("Hello" + "World")
@@ -16,7 +17,7 @@ class ForComprehensionsSpec extends Specification {
       // Evaluate using map, flatMap and filter
       val fmFuture = ForComprehensions.flatMapExpression(f1, f2, f3, test)
       // concatenate strings, perform addition, multiply length, ensure it is divisible by 2
-      Await.result(fmFuture, 0 nanos) mustEqual(30)
+      Await.result(fmFuture, new FiniteDuration(1, TimeUnit.SECONDS)) mustEqual(30)
     }
   }
 
@@ -24,7 +25,7 @@ class ForComprehensionsSpec extends Specification {
     "evaluate to 30" in {
       // The same evaluation using a for comprehension
       val fmFuture = ForComprehensions.forExpression(f1, f2, f3, test)
-      Await.result(fmFuture, 0 nanos) mustEqual(30)
+      Await.result(fmFuture, new FiniteDuration(1, TimeUnit.SECONDS)) mustEqual(30)
     }
   }
 }
