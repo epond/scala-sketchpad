8a3a0bbf68820d5e3060c3a6680e9663c6b8fa81
Ed Pond <edward.pond@springer.com>
2015-01-26 13:31:18 +0000

    Improve PartialFunctions

--- src/main/scala/sketchpad/core/PartialFunctions.scala
+++ src/main/scala/sketchpad/core/PartialFunctions.scala
@@ -1,16 +1,23 @@
 package sketchpad.core
 
+// Inspired by http://eed3si9n.com/learning-scalaz/Applicative.html
 object PartialFunctions {
 
-//  ===Haskell===
-//  Prelude> let a = fmap (*) [1,2,3]
-//  Prelude> :t a
-//    a :: Num a => [a -> a]
-//  Prelude> fmap (\f -> f 5) a
-//  [5,10,15]
+  // res1 is a function that takes a function and applies it to 5
+  val res1 = (_: Int => Int)(5)
+  // The equivalent in Haskell: let res1 = (\f -> f 5)
+  // res2 is a function that takes an Int and adds 1 to it
+  val res2 = (x: Int) => x + 1
+  // The equivalent in Haskell: let res2 = (\x -> x + 1)
+  // res3 applies res1 to res2, the result being 6
+  val res3 = res1(res2)
+  // The equivalent in Haskell: let res3 = res1 res2
 
+  // Let's map a function that takes two parameters over a functor
   val a = List(1,2,3) map ((_:Int)*(_:Int)).curried
   // a: List[Int => Int] = List(<function1>, <function1>, <function1>)
+  // The equivalent in Haskell: let a = fmap (*) [1,2,3]
   val b = a map (_(5))
   // b: List[Int] = List(5, 10, 15)
+  // The equivalent in Haskell: let b = fmap (\f -> f 5) a
 }
