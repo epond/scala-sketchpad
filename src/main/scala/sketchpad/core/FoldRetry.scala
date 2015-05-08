5290a9f07a67c7f13c63655b1132dd3965766b4e
Edward Pond <edward.pond@springer.com>
2015-05-08 17:26:49 +0100

    Experimenting with retrying in Try

--- src/main/scala/sketchpad/core/FoldRetry.scala
+++ src/main/scala/sketchpad/core/FoldRetry.scala
@@ -1,6 +1,7 @@
 package sketchpad.core
 
 import scala.concurrent.Future
+import scala.util.{Failure, Try}
 
 object FoldRetry {
 
@@ -29,4 +30,24 @@ object FoldRetry {
     )()
   }
 
+  // How would the above look like for Try?
+  def retryTry[T](noTimes: Int)(block: () => Try[T]): Try[T] = {
+    val attempts: Seq[() => Try[T]] = (1 to noTimes).map(_ => block)
+    val failed = () => Failure(new Exception)
+
+    attempts.foldRight[() => Try[T]] (failed) (
+      (block, acc) =>
+        () => { block() orElse acc() }
+    )()
+  }
+
+  // In link we used this. Needs tests.
+  def retryIt[T](attempts: Int, fun : => Unit) {
+    (1 to attempts).foldLeft(Try(fun))((accumulator, numberOfAttempts) => {
+      accumulator orElse {
+        println(s"Number of attempts: $numberOfAttempts of $attempts")
+        Try(fun)
+      }
+    }).get
+  }
 }
--- src/test/scala/sketchpad/core/FoldRetrySpec.scala
+++ src/test/scala/sketchpad/core/FoldRetrySpec.scala
@@ -4,7 +4,7 @@ import org.specs2.time.NoTimeConversions
 import org.specs2.mutable.Specification
 import scala.concurrent.{Await, Promise, Future}
 import scala.concurrent.duration._
-import scala.util.{Failure, Success}
+import scala.util.{Try, Failure, Success}
 
 class FoldRetrySpec extends Specification with NoTimeConversions {
 
@@ -18,7 +18,7 @@ class FoldRetrySpec extends Specification with NoTimeConversions {
     }
   }
 
-  "A demonstration of fold" should {
+  "A demonstration of fold for Future retries" should {
     "given a block that succeeds on 3 attempts, succeed with max 3 attempts" in {
       val retryFuture = FoldRetry.retry(3)(succeedOnAttempt(3))
       Await.result(retryFuture, 1 seconds) must beEqualTo("woohoo")
@@ -32,6 +32,35 @@ class FoldRetrySpec extends Specification with NoTimeConversions {
     "given a block that succeeds on 3 attempts, succeed with max 4 attempts" in {
       val retryFuture = FoldRetry.retry(4)(succeedOnAttempt(3))
       Await.result(retryFuture, 1 seconds) must beEqualTo("woohoo")
+    }
+  }
+
+  def succeedOnTryAttempt(requiredTries: Int): () => Try[String] = {
+    var tryCount: Int = 0
+    () => {
+      tryCount += 1
+      if (tryCount >= requiredTries) Success("woohoo") else Failure(new Exception("d'oh!"))
+    }
+  }
+
+  "A demonstration of fold for Try retries" should {
+    "given a block that succeeds on 3 attempts, succeed with max 3 attempts" in {
+      val retryTry = FoldRetry.retryTry(3)(succeedOnTryAttempt(3))
+      retryTry.get must beEqualTo("woohoo")
+    }
+
+    "given a block that succeeds on 3 attempts, fail with max 2 attempts" in {
+      val retryTry = FoldRetry.retryTry(2)(succeedOnTryAttempt(3))
+      retryTry.get must throwA[Exception]
+//      retryTry match {
+//        case Failure(ex) if (ex.getMessage.equals("d'oh!")) => ok
+//        case _ => ko
+//      }
+    }
+
+    "given a block that succeeds on 3 attempts, succeed with max 4 attempts" in {
+      val retryTry = FoldRetry.retryTry(4)(succeedOnTryAttempt(3))
+      retryTry.get must beEqualTo("woohoo")
     }
   }
 }
