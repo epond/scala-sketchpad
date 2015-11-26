3c8e9bd0136fae96fabee93ff3f37b7f4f7b566a
Edward Pond <edward.pond@springer.com>
2015-11-26 12:44:34 +0000

    Update xml example

--- src/main/scala/sketchpad/core/ScalaXMLProcessing.scala
+++ src/main/scala/sketchpad/core/ScalaXMLProcessing.scala
@@ -5,4 +5,26 @@ package sketchpad.core
  */
 object ScalaXMLProcessing extends App {
   println(<a><b><d></d></b><b><e></e></b><c/></a> \ "b" \ "e")
+
+  val citationIndexes = <CitationIndexes ShowOnSpringerDotCom="Y">
+    <CitationIndex Year="2009" Rank="50" NoJournals="50" ImpactFactor="1">
+      <Category Term="not defined">ND</Category>
+    </CitationIndex>
+    <CitationIndex Year="2012" Rank="60" NoJournals="60" ImpactFactor="2">
+      <Category Term="not defined">ND</Category>
+    </CitationIndex>
+    <CitationIndex Year="2013" Rank="60" NoJournals="65">
+      <Category Term="not defined">ND</Category>
+    </CitationIndex>
+    <CitationIndex Year="2011" Rank="59" NoJournals="59" ImpactFactor="3">
+      <Category Term="not defined">ND</Category>
+    </CitationIndex>
+  </CitationIndexes>
+
+  println((citationIndexes \ "CitationIndex")
+    .filter(citationIndex => (citationIndex \ "@ImpactFactor").text != "")
+    .sortBy(node => (node \ "@Year").text)
+    .reverse.headOption
+    .map(citationIndex => (citationIndex \ "@ImpactFactor").text)
+  )
 }
