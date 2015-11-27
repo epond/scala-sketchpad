e42afecb182b320b5a00ca29023f67bbc51b6941
Edward Pond <edward.pond@springer.com>
2015-11-27 14:42:08 +0000

    Use Option instead of Future

--- src/main/scala/sketchpad/core/ReaderTransformer.scala
+++ src/main/scala/sketchpad/core/ReaderTransformer.scala
@@ -2,24 +2,22 @@ package sketchpad.core
 
 import sketchpad.core.UserAPI.{APIKey, StreetAddress}
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 import scalaz._
 import Scalaz._
 
 object ReaderTransformer extends App {
   val apiKey = "123ABC"
 
-  val streetAddressDirectly: Future[StreetAddress] = for {
+  val streetAddressDirectly: Option[StreetAddress] = for {
     user <- UserAPI.getUser("Fred", apiKey)
     streetAddress <- UserAPI.getStreetAddress(user.email, apiKey)
   } yield streetAddress
 
-  val streetAddressReader: ReaderT[Future, APIKey, StreetAddress] = for {
+  val streetAddressReader: ReaderT[Option, APIKey, StreetAddress] = for {
     user <- UserReaderAPI.getUser("Fred")
     streetAddress <- UserReaderAPI.getStreetAddress(user.email)
   } yield streetAddress
-  val streetAddressFromReader: Future[StreetAddress] = streetAddressReader(apiKey)
+  val streetAddressFromReader: Option[StreetAddress] = streetAddressReader(apiKey)
 }
 
 case class User(name: UserAPI.Name, email: UserAPI.Email)
@@ -30,21 +28,21 @@ object UserAPI {
   type StreetAddress = String
   type APIKey = String
 
-  def getUser(name: Name, apiKey: APIKey): Future[User] =
-    Future(User("Fred", "fred@mail.com"))
-  def getStreetAddress(email: String, apiKey: APIKey): Future[StreetAddress] =
-    Future("1 Acacia Road")
+  def getUser(name: Name, apiKey: APIKey): Option[User] =
+    Some(User("Fred", "fred@mail.com"))
+  def getStreetAddress(email: String, apiKey: APIKey): Option[StreetAddress] =
+    Some("1 Acacia Road")
 }
 
 object UserReaderAPI {
   import Kleisli._
 
-  def getUser(name: UserAPI.Name): ReaderT[Future, UserAPI.APIKey, User] = {
+  def getUser(name: UserAPI.Name): ReaderT[Option, UserAPI.APIKey, User] = {
     kleisli { apiKey =>
       UserAPI.getUser(name, apiKey)
     }
   }
-  def getStreetAddress(email: String): ReaderT[Future, UserAPI.APIKey, UserAPI.StreetAddress] = {
+  def getStreetAddress(email: String): ReaderT[Option, UserAPI.APIKey, UserAPI.StreetAddress] = {
     kleisli { apiKey =>
       UserAPI.getStreetAddress(email, apiKey)
     }
