2cf1a0b229f9305ec1f72db727a6ad86a0b5086f
epond <epond@users.noreply.github.com>
2016-05-17 08:04:03 +0100

    FuturesOfEither: more pending tests

--- src/test/scala/sketchpad/bettereither/FutureOfEitherSpec.scala
+++ src/test/scala/sketchpad/bettereither/FutureOfEitherSpec.scala
@@ -9,22 +9,30 @@ import scala.concurrent.duration._
 class FutureOfEitherSpec extends Specification {
   "render page" should {
     "contain article and contact list" in {
-      pending
+      val lookup = DummyLookup(\/-(Article("123")), \/-(User("Dave")), \/-(List(User("Rod"), User("Jane"))))
+      val page: Future[PageRendering] = FuturesOfEither.renderPage("", "", lookup)
+      val expectedRendering = "Article(123),List(User(Rod), User(Jane))"
+      Await.result(page, Duration(1, SECONDS)) must beEqualTo(PageRendering(expectedRendering))
     }
     "contain article and empty contact list when contacts are empty" in {
-      val lookup = DummyLookup(Article("123"), User("Dave"), List())
+      val lookup = DummyLookup(\/-(Article("123")), \/-(User("Dave")), \/-(List()))
       val page: Future[PageRendering] = FuturesOfEither.renderPage("", "", lookup)
       val expectedRendering = "Article(123),List()"
       Await.result(page, Duration(1, SECONDS)) must beEqualTo(PageRendering(expectedRendering))
     }
-    "contain article and empty contact list when user not found" in {
+    "contain article and empty contact list when user lookup fails" in {
+      pending
+    }
+    "fail when article lookup fails" in {
       pending
     }
   }
 }
 
-case class DummyLookup(article: Article, user: User, contacts: Seq[User]) extends Lookup {
-  override def lookupArticle(id: String): Future[\/[String, Article]] = Future.successful(\/-(article))
-  override def lookupUser(name: String): Future[\/[String, User]] = Future.successful(\/-(user))
-  override def lookupContacts(user: \/[String, User]): Future[\/[String, Seq[User]]] = Future.successful(\/-(contacts))
+case class DummyLookup(article: String \/ Article,
+                       user: String \/ User,
+                       contacts: String \/ Seq[User]) extends Lookup {
+  override def lookupArticle(id: String): Future[\/[String, Article]] = Future.successful(article)
+  override def lookupUser(name: String): Future[\/[String, User]] = Future.successful(user)
+  override def lookupContacts(user: \/[String, User]): Future[\/[String, Seq[User]]] = Future.successful(contacts)
 }
