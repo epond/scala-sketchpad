f2d76fd996f87bdac724244cdff2e8d38a5f2aed
epond <epond@users.noreply.github.com>
2017-04-26 19:37:22 +0100

    explicit and implicit typeclass evidence

--- src/test/scala/sketchpad/typeclass/Typeclass2Spec.scala
+++ src/test/scala/sketchpad/typeclass/Typeclass2Spec.scala
@@ -1,20 +1,31 @@
 package sketchpad.typeclass
 
 import org.specs2.mutable.Specification
-import sketchpad.typeclass.Fuel.{Diesel, Petrol}
+import Fuel.{Diesel, Petrol}
 
 class Typeclass2Spec extends Specification {
-  "The Weighable typeclass" should {
+  "The Weighable typeclass when used explicitly" should {
+    "find the weight of a Petrol Car" in {
+      WeighableEvidence.WeighableCar.weight(Car(Petrol)) must beEqualTo(20)
+    }
+    "find the weight of a Diesel Car" in {
+      WeighableEvidence.WeighableCar.weight(Car(Diesel)) must beEqualTo(22)
+    }
+    "find the weight of a Motorbike" in {
+      WeighableEvidence.WeighableMotorbike.weight(Motorbike) must beEqualTo(10)
+    }
+    "find the combined weight of two Petrol Cars" in {
+      Scales.sumWeighable(Car(Petrol), Car(Petrol))(WeighableEvidence.WeighableCar, WeighableEvidence.WeighableCar) must beEqualTo(40)
+    }
+    "find the combined weight of two different Cars" in {
+      Scales.sumWeighable(Car(Petrol), Car(Diesel))(WeighableEvidence.WeighableCar, WeighableEvidence.WeighableCar) must beEqualTo(42)
+    }
+    "find the combined weight of two Weighable things" in {
+      Scales.sumWeighable(Car(Diesel), Motorbike)(WeighableEvidence.WeighableCar, WeighableEvidence.WeighableMotorbike) must beEqualTo(32)
+    }
+  }
+  "The Weighable typeclass when used implicitly" should {
     import WeighableEvidence._
-//    "find the weight of a Petrol Car" in {
-//      Car(Petrol).weight must beEqualTo(20)
-//    }
-//    "find the weight of a Diesel Car" in {
-//      Car(Diesel).weight must beEqualTo(22)
-//    }
-//    "find the weight of a Motorbike" in {
-//      Motorbike.weight must beEqualTo(10)
-//    }
     "find the combined weight of two Petrol Cars" in {
       Scales.sumWeighable(Car(Petrol), Car(Petrol)) must beEqualTo(40)
     }
